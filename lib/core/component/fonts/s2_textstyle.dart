391c4f0677e961f22deed8146ba6e1c88234b44e
seoyerin <us3544974@gmail.com>
2024-12-27 21:06:33 +0900

    FEAT :: textstyle 추가

---
+++ lib/core/component/fonts/s2_textstyle.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const String pretendard = 'Pretendard';
+
+class S2TextStyle{
+  static TextStyle semiBold30({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 30,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle 
+}
