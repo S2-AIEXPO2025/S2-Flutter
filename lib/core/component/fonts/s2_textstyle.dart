b6579c44bbb77ceffb8d10def3e2e86062af123d
seoyerin <us3544974@gmail.com>
2024-12-28 20:29:14 +0900

    FEAT :: TextStyle 추가

--- lib/core/component/fonts/s2_textstyle.dart
+++ lib/core/component/fonts/s2_textstyle.dart
@@ -15,5 +15,63 @@ class S2TextStyle{
     );
   }
 
-  static TextStyle 
+  static TextStyle regular30({
+    required Color color
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 30,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.visible
+    );
+  }
+
+  static TextStyle medium20({
+    required Color color,
+  }){
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible
+    );
+  }
+
+  static TextStyle medium15({
+    required Color color
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 15,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible
+    );
+  }
+
+  static TextStyle medium13({
+    required Color color
+  }) {
+    return TextStyle(
+        color: color,
+        fontSize: 13,
+        fontFamily: pretendard,
+        fontWeight: FontWeight.w500,
+        overflow: TextOverflow.visible
+    );
+  }
+
+  static TextStyle regular10({
+    required Color color
+  }) {
+    return TextStyle(
+        color: color,
+        fontSize: 10,
+        fontFamily: pretendard,
+        fontWeight: FontWeight.w400,
+        overflow: TextOverflow.visible
+    );
+  }
 }
