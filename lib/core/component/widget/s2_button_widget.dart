1f6521da8c2a4848fd749899d556ab07b1415c77
seoyerin <us3544974@gmail.com>
2024-12-31 20:44:38 +0900

    [#3] 시작하기 버튼 위젯 추가

---
+++ lib/core/component/widget/s2_button_widget.dart
@@ -0,0 +1,37 @@
+import 'package:expos2/core/constants/s2_style.dart';
+import 'package:expos2/core/di/router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class S2ButtonWidget extends StatelessWidget {
+
+  final Color color;
+  final String text;
+  final Color? backgroundColor;
+
+  const S2ButtonWidget({
+    required this.color,
+    required this.text,
+    required this.backgroundColor,
+    super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        context.push('/');//나중에 채우기
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 62,
+        decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(10)
+        ),
+        child: Center(
+          child: Text(text, style: S2TextStyle.medium20(color: color),),
+        ),
+      ),
+    );
+  }
+}
