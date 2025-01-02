8e21fbad8d17240648cf0b69bbcd512738d7cfe1
seoyerin <us3544974@gmail.com>
2025-01-02 19:59:45 +0900

    FEAT :: component 수정

--- lib/core/component/widget/s2_start_button_widget.dart
+++ lib/core/component/widget/s2_start_button_widget.dart
@@ -1,35 +1,36 @@
 import 'package:expos2/core/constants/s2_style.dart';
-import 'package:expos2/core/di/router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class S2StartButtonWidget extends StatelessWidget {
-
   final Color color;
   final String text;
   final Color? backgroundColor;
+  final VoidCallback onPressed;
 
   const S2StartButtonWidget({
     required this.color,
     required this.text,
     required this.backgroundColor,
-    super.key});
+    required this.onPressed,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        context.push('/login');//나중에 채우기
-      },
+      onTap: onPressed,
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 62,
         decoration: BoxDecoration(
-            color: backgroundColor,
-            borderRadius: BorderRadius.circular(10)
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10),
         ),
         child: Center(
-          child: Text(text, style: S2TextStyle.medium20(color: color),),
+          child: Text(
+            text,
+            style: S2TextStyle.medium20(color: color),
+          ),
         ),
       ),
     );
