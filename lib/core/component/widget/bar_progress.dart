31465567c760e57f6b1f929f231f454de3c341f8
seoyerin <us3544974@gmail.com>
2025-01-03 19:43:08 +0900

    [#14] progress bar 구현

---
+++ lib/core/component/widget/bar_progress.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class BarProgress extends StatelessWidget {
+  final double percentage;
+  final Color backColor;
+  final bool showPercentage;
+  final double stroke;
+  final bool round;
+  final Color borderColor;
+  final Color progressColor;
+
+  const BarProgress({
+    Key? key,
+    required this.percentage,
+    required this.backColor,
+    required this.showPercentage,
+    required this.stroke,
+    required this.round,
+    this.borderColor = Colors.pink,
+    this.progressColor = Colors.pink,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: stroke,
+      decoration: BoxDecoration(
+        color: backColor,
+        borderRadius: round ? BorderRadius.circular(stroke / 2) : null,
+        border: Border.all(color: borderColor, width: 1),
+      ),
+      child: Stack(
+        children: [
+          Container(
+            width: percentage / 100 * MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: progressColor,
+              borderRadius: round ? BorderRadius.circular(stroke / 2) : null,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
