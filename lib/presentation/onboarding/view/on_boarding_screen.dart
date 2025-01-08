0f60b480e6dde64d3b0ab55fbc6bc32aaf59af26
seoyerin <us3544974@gmail.com>
2025-01-08 21:56:58 +0900

    [#22] UI 수정

--- lib/presentation/onboarding/view/on_boarding_screen.dart
+++ lib/presentation/onboarding/view/on_boarding_screen.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:expos2/core/component/fonts/s2_textstyle.dart';
-import 'package:expos2/core/component/widget/s2_button_widget.dart';
 import 'package:expos2/core/component/widget/s2_start_button_widget.dart';
 import 'package:expos2/core/constants/s2_color.dart';
 import 'package:flutter/material.dart';
@@ -22,14 +21,23 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
       children: [
         Row(
           children: [
-            Text('data', style: S2TextStyle.ExtraBold40(color: S2Color.black)),            const SizedBox(width: 4),            Image.asset('assets/images/icons/onboarding/heart_icons.png'),          ],),
-    const SizedBox(height: 68),
-    Text(
-      'AI 연인과 채팅으로\n편하게 연애의 경험을\n시작합니다. 취향에 맞는 AI를\n통해 연애 경험을 쌓습니다.',
-      style: S2TextStyle.medium20(color: S2Color.black),
-    ),
-    const SizedBox(height: 90),
-    Center(child: Image.asset('assets/images/icons/onboarding/main.png')),
+            Text('data', style: S2TextStyle.ExtraBold40(color: S2Color.black)),
+            const SizedBox(width: 4),
+            Image.asset('assets/images/icons/onboarding/heart_icons.png'),],),
+        const SizedBox(height: 68),
+        Text(
+          'AI 연인과 채팅으로\n편하게 연애의 경험을\n시작합니다. 취향에 맞는 AI를', 
+          style: S2TextStyle.medium20(color: S2Color.black),
+        ),
+        Row(
+          children: [
+            Text('통해 ', style: S2TextStyle.medium20(color: S2Color.black),),
+            Text('연애 경험', style: S2TextStyle.medium20(color: S2Color.pink),),
+            Text('을 쌓습니다.',style: S2TextStyle.medium20(color: S2Color.black),)
+          ],
+        ),
+        const SizedBox(height: 90), 
+        Center(child: Image.asset('assets/images/icons/onboarding/main.png')),
   ],
   ),
     Column(
@@ -43,10 +51,25 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
           ],
         ),
         const SizedBox(height: 68),
+        Row(
+          children: [
+            Text('AI 채팅으로', style: S2TextStyle.medium20(color: S2Color.black),),
+            Text(' 빠르게 연결', style: S2TextStyle.medium20(color: S2Color.pink),),
+            Text('되는', style: S2TextStyle.medium20(color: S2Color.black),)
+          ],
+        ),
         Text(
-          'AI 채팅으로 빠르게 연결되는\n대화를 할 수 있습니다.\nAI 연인과 24시간 언제든 얘기를\n나눌 수 있습니다.',
+          '대화를 할 수 있습니다.',
           style: S2TextStyle.medium20(color: S2Color.black),
         ),
+        Row(
+          children: [
+            Text('AI 연인과', style: S2TextStyle.medium20(color: S2Color.black),),
+            Text(' 24시간 언제든', style: S2TextStyle.medium20(color: S2Color.pink),),
+            Text(' 얘기를', style: S2TextStyle.medium20(color: S2Color.black),)
+          ],
+        ),
+        Text('나눌 수 있습니다.', style: S2TextStyle.medium20(color: S2Color.black),),
         const SizedBox(height: 90),
         Center(child: Image.asset('assets/images/icons/onboarding/main.png')),
       ],
@@ -63,9 +86,23 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         ),
         const SizedBox(height: 68),
         Text(
-          'AI 연인을 통해 연애할 때의\n감정 그대로를 느껴보실 수 있습니다.\n실제 연애할 때의 감정을\n느끼고 연습할 수 있습니다.',
+          'AI 연인을 통해 연애할 때의',
           style: S2TextStyle.medium20(color: S2Color.black),
         ),
+        Row(
+          children: [
+            Text('감정 그대로', style: S2TextStyle.medium20(color: S2Color.pink),),
+            Text('를 느껴보실 수 있습니다.', style: S2TextStyle.medium20(color: S2Color.black),)
+          ],
+        ),
+        Row(
+          children: [
+            Text('실제 연애할 때의 ', style: S2TextStyle.medium20(color: S2Color.black),),
+            Text('감정', style: S2TextStyle.medium20(color: S2Color.pink),),
+            Text('을', style: S2TextStyle.medium20(color: S2Color.black),)
+          ],
+        ),
+        Text('느끼고 연습할 수 있습니다.', style: S2TextStyle.medium20(color: S2Color.black),),
         const SizedBox(height: 90),
         Center(child: Image.asset('assets/images/icons/onboarding/main.png')),
       ],
