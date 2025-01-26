242cb0a63b2a8b0c7dcad466ac8334b57cc0fb59
seoyerin <us3544974@gmail.com>
2025-01-26 23:28:16 +0900

    FEAT :: 컬러 추가

--- lib/core/constants/s2_color.dart
+++ lib/core/constants/s2_color.dart
@@ -6,12 +6,17 @@ class S2Color{
   static Color black = const Color(0XFF000000);
 
   static Color gray01 = const Color(0xFFDCDCDC);
-  static Color green01 = const Color(0xFF1ADE3E);
+  static Color gray02 = const Color(0xFFC2C2C2);
+  static Color gray03 = const Color(0xFFE4E4E4);
+  static Color gray04 = const Color(0xFFF3F3F3);
+  static Color gray05 = const Color(0xFFF9F9F9);
+  static Color green = const Color(0xFF1CD13D);
   static Color pink = const Color(0xFFF73A93);
-  static Color green = const Color(0xFFE5F1CA);
+  static Color maingreen = const Color(0xFFE5F1CA);
   static Color backgroundpink = const Color(0xFFFEEBF4);
+  static Color stratbackgroundcolor = const Color(0xFFF2EDF0);
 
-  static Gradient main = const LinearGradient(
+  static Gradient pinkgreen = const LinearGradient(
     colors: [
       Color(0xFFF73A93),
       Color(0xFFE5F1CA)
