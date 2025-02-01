431fc4d3486873a1c908fba8f2497de2df03d4dd
seoyerin <us3544974@gmail.com>
2025-02-01 20:29:50 +0900

    FEAT :: 메인 페이지 스크린 구현

--- lib/presentation/home/view/main_screen.dart
+++ lib/presentation/home/view/main_screen.dart
@@ -19,28 +19,31 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       backgroundColor: S2Color.stratbackgroundcolor,
       appBar: const MainScreenAppBarWidget(),
-      body: Column(
-        children: [
-          const SizedBox(height: 12),
-          const MainScreenBannerWidget(),
-          Padding(padding: const EdgeInsets.only(left: 23, right: 23, top: 23, bottom: 14),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text('‘가나다라마사아자차카타파하\'님을 위한 AI 연인 추천',
-                      style: S2TextStyle.medium15(color: S2Color.black),
-                    ),
-                    const SizedBox(width: 4),
-                    SvgPicture.asset('assets/images/icons/main/glow_icons.svg')
-                  ],
-                ),
-                const SizedBox(height: 12),
-                MainScreenSliderWidget()
-              ],
-            ),
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 12),
+            const MainScreenBannerWidget(),
+            Padding(padding: const EdgeInsets.only(left: 23, right: 23, top: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('‘가나다라마사아자차카타파하\'님을 위한 AI 연인 추천',
+                        style: S2TextStyle.medium15(color: S2Color.black),
+                      ),
+                      const SizedBox(width: 4),
+                      SvgPicture.asset('assets/images/icons/main/glow_icons.svg')
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  MainScreenSliderWidget(),
+                  const SizedBox(height: 12,)
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
