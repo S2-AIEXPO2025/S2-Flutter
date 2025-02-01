eb9f2f383ac0e02fe2c42004411e508bfb196c32
seoyerin <us3544974@gmail.com>
2025-02-01 21:19:47 +0900

    CHORE ::

---
+++ lib/presentation/chating/view/chat_screen.dart
@@ -0,0 +1,18 @@
+import 'package:expos2/core/constants/s2_color.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: S2Color.white,
+    );
+  }
+}
