394bc5f58bc3f346f7001d675f1eeca4994412c4
seoyerin <us3544974@gmail.com>
2025-02-03 08:40:03 +0900

    [#31] 채팅 스크린 바디 위젯 추가

---
+++ lib/presentation/chating/widget/chat_screen_body_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatScreenBodyWidget extends StatefulWidget {
+  const ChatScreenBodyWidget({super.key});
+
+  @override
+  State<ChatScreenBodyWidget> createState() => _ChatScreenBodyWidgetState();
+}
+
+class _ChatScreenBodyWidgetState extends State<ChatScreenBodyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
