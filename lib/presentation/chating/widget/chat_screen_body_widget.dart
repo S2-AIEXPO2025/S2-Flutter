78eea2e34241edc3d47a17b745cca5d55f1fa318
seoyerin <us3544974@gmail.com>
2025-02-03 16:31:12 +0900

    [#31] 예외 처리 구현

--- lib/presentation/chating/widget/chat_screen_body_widget.dart
+++ lib/presentation/chating/widget/chat_screen_body_widget.dart
@@ -1,3 +1,5 @@
+import 'package:expos2/core/component/fonts/s2_textstyle.dart';
+import 'package:expos2/core/constants/s2_color.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreenBodyWidget extends StatefulWidget {
@@ -12,7 +14,10 @@ class _ChatScreenBodyWidgetState extends State<ChatScreenBodyWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-
+        const SizedBox(height: 100),
+        Center(
+          child:Text('아직 채팅한 기록이 없습니다.', style: S2TextStyle.regular13(color: S2Color.gray01),)
+        )
       ],
     );
   }
