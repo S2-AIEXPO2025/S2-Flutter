191250cfc33a2f9b839257b1d5f8b427b0f48afc
seoyerin <us3544974@gmail.com>
2025-02-04 15:39:32 +0900

    [#33] 채팅 세부 페이지 구현

---
+++ lib/presentation/chating_detail/view/chating_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:expos2/core/constants/s2_color.dart';
+import 'package:flutter/material.dart';
+
+class ChatingDetailScreen extends StatefulWidget {
+  const ChatingDetailScreen({super.key});
+
+  @override
+  State<ChatingDetailScreen> createState() => _ChatingDetailScreenState();
+}
+
+class _ChatingDetailScreenState extends State<ChatingDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: S2Color.backgroundpink,
+    );
+  }
+}
