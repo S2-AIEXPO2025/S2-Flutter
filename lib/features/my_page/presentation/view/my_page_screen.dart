45461a38972868ca5c460d65ee21672b1a16b871
seoyerin <us3544974@gmail.com>
2025-02-10 23:38:23 +0900

    [#38] 마이페이지 스크린 구현

--- lib/features/my_page/presentation/view/my_page_screen.dart
+++ lib/features/my_page/presentation/view/my_page_screen.dart
@@ -1,4 +1,8 @@
 import 'package:expos2/core/s2_color.dart';
+import 'package:expos2/core/s2_style.dart';
+import 'package:expos2/features/my_page/presentation/widget/my_page_app_bar_widget.dart';
+import 'package:expos2/features/my_page/presentation/widget/my_page_info_text_field_widget.dart';
+import 'package:expos2/features/my_page/presentation/widget/my_page_user_info_widget.dart';
 import 'package:flutter/material.dart';
 
 class MyPageScreen extends StatelessWidget {
@@ -8,6 +12,18 @@ class MyPageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: S2Color.white,
+      appBar: const MyPageAppBarWidget(),
+      body: Padding(padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 8),
+            Text('회원정보', style: S2TextStyle.medium15(color: S2Color.black),),
+            MyPageUserInfoWidget(),
+            MyPageInfoTextFieldWidget()
+          ],
+        ),
+      ),
     );
   }
 }
