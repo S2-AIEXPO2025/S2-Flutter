7850c847046cb3310d09c788eb9f27ddca092bb4
seoyerin <us3544974@gmail.com>
2025-02-11 22:56:10 +0900

    [#38] 바텀 위젯 추가

--- lib/features/my_page/presentation/widget/my_page_bottom_widget.dart
+++ lib/features/my_page/presentation/widget/my_page_bottom_widget.dart
@@ -6,14 +6,40 @@ class MyPageBottomWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: (){},
-          child: Center(child: Text('로그아웃', style: S2TextStyle.medium13(color: S2Color.error),)),
-        ),
-        const SizedBox(height: 41),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                onTap: (){},
+                child: Text('프로필 정보 수정',
+                  style: S2TextStyle.medium13(
+                    decoration: TextDecoration.underline,
+                    color: S2Color.gray02
+                  )
+                ),
+              ),
+              GestureDetector(
+                onTap: (){},
+                child: Text('비밀번호 변경',
+                  style: S2TextStyle.medium13(
+                      color: S2Color.gray02,
+                      decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 48),
+          GestureDetector(
+            onTap: (){},
+            child: Center(child: Text('로그아웃', style: S2TextStyle.medium13_2(color: S2Color.error),)),
+          ),
+          const SizedBox(height: 41),
+        ],
+      ),
     );
   }
 }
